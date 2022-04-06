2dd5c12c5838577e0f4583f4e37bfa1574f7865d
fed1k <allamurotovcoder@mail.ru>
2022-04-06 21:35:54 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as doc from './scoreList.js';
 import './style.css';
+
 const section = document.querySelector('.scoreList');
 section.appendChild(doc.listBoard);
--- src/scoreList.js
+++ src/scoreList.js
@@ -1,13 +1,13 @@
 const listBoard = document.createElement('div');
 const array = [
-  { name: "Firdavs", score: 91 },
-  { name: "Said", score: 69 },
-  { name: "Sediq", score: 51 },
-  { name: "Amedzro", score: 48 },
+  { name: 'Firdavs', score: 91 },
+  { name: 'Said', score: 69 },
+  { name: 'Sediq', score: 51 },
+  { name: 'Amedzro', score: 48 },
 ];
 
-for (let i of array) {
-  const p = document.createElement("p");
+for (const i of array) {
+  const p = document.createElement('p');
   p.textContent = `${i.name}: ${i.score}`;
   listBoard.appendChild(p);
 }
